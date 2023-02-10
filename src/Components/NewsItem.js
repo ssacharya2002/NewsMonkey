62eaa04ab0120a7ab4bdac8f31692184f191f15f
ssacharya <ssacharya@gmail.com>
2023-02-10 23:07:26 +0530

    mapin throughout the jsx

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
 
-  constructor(){
-    super();
-    console.log("hello im a constructor");
-
-  }
-
-
-
   render() {
-    let {title , description , imgUrl, NewsUrl} = this.props;
+    let { title, description, imgUrl, NewsUrl } = this.props;
     return (
-      <div className="card my-3" style={{width: "18rem"}}>
-      <img src={imgUrl} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        <a href={NewsUrl} className="btn btn-primary btn-sm">Read more</a>
+      <div className="card my-3" style={{ width: "18rem" }}>
+        <img src={imgUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href={NewsUrl} target="_blank" className="btn btn-primary btn-sm">
+            Read more
+          </a>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
